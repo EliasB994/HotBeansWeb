427eed83c4d51dc735f4c9e83ec11a5f8cd07311
EliasB994 <eliasbielskis08@gmail.com>
2025-07-02 14:51:20 +0100

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -21,25 +21,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
-
-// src/components/Footer.jsx
-import React from 'react';
-import '../styles.css';
-
-export default function Footer() {
-  return (
-    <footer>
-      <div className="container text-center">
-        <div className="footer-links">
-          <a href="/">Home</a>
-          <a href="/about">About us</a>
-          <a href="/staff">Staff Profiles</a>
-          <a href="/courses">Web Courses</a>
-          <a href="/apply">Application form</a>
-        </div>
-        <p className="mt-4">© {new Date().getFullYear()} Hot Beans Web. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
