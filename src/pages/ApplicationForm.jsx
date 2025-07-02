71c54bf2902f1801bac8e96d831d3d5084428586
EliasB994 <eliasbielskis08@gmail.com>
2025-07-02 14:56:28 +0100

    Create ApplicationForm.jsx

---
+++ src/pages/ApplicationForm.jsx
@@ -0,0 +1,13 @@
+// src/pages/ApplicationForm.jsx
+import React from 'react';
+import '../styles.css';
+
+export default function ApplicationForm() {
+  return (
+    <main className="container mt-4">
+      <h1>Application Form</h1>
+    <script type="text/javascript" src="https://form.jotform.com/jsform/251673401019349"></script>
+      <section className="form-placeholder mt-4"></section>
+    </main>
+  );
+}
