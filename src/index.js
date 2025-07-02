542bba8bc00358200e952cb8a90b1403ce6572ff
EliasB994 <eliasbielskis08@gmail.com>
2025-07-02 15:08:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,33 +15,6 @@ root.render(
 
 
 
-/* src/App.jsx */
-import React from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import AppRoutes from './routes';
-import './App.css';
-
-export default function App() {
-  const location = useLocation();
-
-  return (
-    <>
-      <Navbar />
-      <TransitionGroup>
-        <CSSTransition key={location.key} classNames="fade" timeout={500}>
-          <div className="page-content">
-            <AppRoutes />
-          </div>
-        </CSSTransition>
-      </TransitionGroup>
-      <Footer />
-    </>
-  );
-}
-
 
 /* src/App.css */
 .page-content {
