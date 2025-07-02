c5eb1913a3df1614b10752b3d579930ebd4bf013
EliasB994 <eliasbielskis08@gmail.com>
2025-07-02 15:00:12 +0100

    Update ApplicationForm.jsx

--- src/pages/ApplicationForm.jsx
+++ src/pages/ApplicationForm.jsx
@@ -1,13 +1,27 @@
 // src/pages/ApplicationForm.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles.css';
 
-export default function ApplicationForm() {
+const ApplicationForm = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://form.jotform.com/jsform/251673401019349';
+    script.type = 'text/javascript';
+    script.async = true;
+    document.getElementById('jotform-form-container').appendChild(script);
+  }, []);
+
   return (
     <main className="container mt-4">
       <h1>Application Form</h1>
-    <script type="text/javascript" src="https://form.jotform.com/jsform/251673401019349"></script>
-      <section className="form-placeholder mt-4"></section>
+      <section
+        id="jotform-form-container"
+        className="form-placeholder mt-4"
+      >
+        {/* JotForm will inject the form here */}
+      </section>
     </main>
   );
-}
+};
+
+export default ApplicationForm;
