3211d3873a68effd0327c72a5191ddc9f6b9ff80
EliasB994 <eliasbielskis08@gmail.com>
2025-07-02 15:07:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,27 +14,6 @@ root.render(
 );
 
 
-/* src/routes.js */
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';
-import StaffProfiles from './pages/StaffProfiles';
-import WebCourses from './pages/WebCourses';
-import ApplicationForm from './pages/ApplicationForm';
-
-export default function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<AboutUs />} />
-      <Route path="/staff" element={<StaffProfiles />} />
-      <Route path="/courses" element={<WebCourses />} />
-      <Route path="/apply" element={<ApplicationForm />} />
-    </Routes>
-  );
-}
-
 
 /* src/App.jsx */
 import React from 'react';
